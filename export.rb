636da30c7803c68f293df03360cbd2c14b481507
‘MengRu <ru.han@openapply.com>
2020-07-24 15:26:22 +0800

    no message

--- export.rb
+++ export.rb
@@ -9,17 +9,20 @@ TOKEN = ENV['TOKEN']
 response = HTTParty.get("https://faria.openapply.com/api/v3/students?access_token=#{TOKEN}&per_page=10", format: :plain)
 JSON.parse response, symbolize_names: true
 obj = JSON.parse response
-puts obj.class
-ss = obj["students"]
-puts ss
-    
-column_names = ss.first.keys
-s=CSV.generate do |csv|
-  csv << column_names
-  ss.each do |x|
+# puts obj.class
+i = obj["students"]
+
+
+
+
+
+    column = i.first.keys     # 散列的第一个列
+s=CSV.generate do |csv| 
+  csv << column
+  i.each do |x|    
     csv << x.values
   end
 end
-File.write('all_info.csv', s)
+File.write('test04.csv', s)
 
 
